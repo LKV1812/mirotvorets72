19a7b7626e2f777b5f5b4bc03224e90ed394ef7b
Konstantin <Konstantin.ledik@mail.ru>
2020-07-30 16:46:22 +0500

    add content in page vacancy

--- src/js/modules/set-selection.js
+++ src/js/modules/set-selection.js
@@ -8,7 +8,12 @@ responseButton.onclick = function() {
   setSelection(parentVacancy, '.vacancy-body.vacancy-body--is-active .vacancy-body__title', inputProfession);
 };
 
-
+/**
+ * setSelection() - вычитывет строку с заголовоком вакансии и записывет подстроку в форму отклика, плавно прокручивает страницу до формы
+ * @param {DOM} parent - родитель внутри которого ищем элемент со строкой
+ * @param {DOM} desiredElem - элемент из которого вырезаем подстроку
+ * @param {DOM} toElem - элемент в, который вставляем подстроку
+ */
 export function setSelection(parent, desiredElem, toElem) {
   let text = getSubstring( parent.querySelector(desiredElem) );
   let responseSection = document.getElementById('responseSection');
