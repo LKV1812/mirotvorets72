9640fef14573bd3a8f4bfb231969877523cfd6ae
Konstantin <Konstantin.ledik@mail.ru>
2020-09-23 21:27:31 +0500

    basket ih modal window ready

--- src/js/pallets.js
+++ src/js/pallets.js
@@ -1,1 +1,7 @@
 import './entry';
+import './modules/basket';
+import {callsModalWindow} from './modules/modal-window';
+
+let buttonOpenModalWindow = document.querySelector('.basket');
+let modalWindowQuestion = document.getElementById('palletsBasket');
+callsModalWindow(buttonOpenModalWindow, modalWindowQuestion);
